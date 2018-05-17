e61853cf96e82dfe8f4bc7dab0f8375506b7b4a7
gitpriyachaudhary <priya.chaudhary@aimbeyond.com>
2018-05-17 17:50:36 +0530

    addSkills Changes

---
+++ addSkillsSource.php
@@ -0,0 +1,13 @@
+<?php 
+if(isset($_POST['submit']))
+{
+$skill= $_POST['skill'];
+
+$sql= "INSERT INTO SKILL(SKILL_NAME) VALUES ('$skill')";
+$run=mysqli_query($conn,$sql);
+}
+if($run)
+{
+	echo "<script type= 'text/javascript'> alert('Skill Inserted Successfully'); </script>";
+} 
+?>
