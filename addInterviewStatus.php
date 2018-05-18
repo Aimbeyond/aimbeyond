38c04b9021259a4180ead7a44d74ad040aadff31
gitpriyachaudhary <priya.chaudhary@aimbeyond.com>
2018-05-18 12:51:37 +0530

    addRound changes

--- addInterviewStatus.php
+++ addInterviewStatus.php
@@ -57,7 +57,7 @@ include("header.php");
                                          <button type="submit" class="cancelmaster">CANCEL</button>
                                 </div>
                                 <div class="col-lg-6 col-md-6 col-sm-6">   
-                                    <button type="submit" class="submitmaster ">SUBMIT </button>
+                                    <button type="submit" class="submitmaster " name="submit">SUBMIT </button>
                                 </div>
                             </div>
                         </form>
