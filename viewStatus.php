f722070f6610bf4893a59dce1d7ebd3ad2fe34ca
gitdivyanshu <divyanshubhardwaj4gmail.com>
2018-05-21 14:23:48 +0530

    view applicant status

--- viewStatus.php
+++ viewStatus.php
@@ -1,5 +1,10 @@
 <?php
 include("header.php");
+            $sql = 'SELECT * FROM APPLICANT_STATUS';
+
+            $result = mysqli_query($conn, $sql);
+
+
 ?>
 
     <!-- Right Panel -->
@@ -43,14 +48,46 @@ include("header.php");
                       </tr>
                     </thead>
                     <tbody>
+                              <?php 
+                                $i=1;
+                                while($data=mysqli_fetch_array($result))
+                                {
+                                  
+                                ?>
                       <tr>
                      
-                          <td>1</td>
-                          <td>1</td>
-                          <td>Amit Rana</td>
-                          <td class="email-link">amit@gmail.com</td>
-                          <td>2 yrs</td>
-                          <td>898978979</td>
+                          <td><?php echo  $i; ?></td>
+                          <?php 
+                          $sqlDetail = "SELECT * FROM APPLICANT_DETAIL WHERE REG_ID='".$data['REG_ID']."'";
+
+                          $resultDetail = mysqli_query($conn, $sqlDetail);
+                          $rowDetail = mysqli_fetch_array($resultDetail)
+                          ?>
+
+                          <td><?php echo $rowDetail['APPLICANT_NAME']; ?></td>
+                          <?php 
+                          $sqlJobDetail = "SELECT * FROM JOB_DETAIL WHERE JOB_ID='".$data['JOB_ID']."'";
+
+                          $resultJobDetail = mysqli_query($conn, $sqlJobDetail);
+                          $rowJobDetail = mysqli_fetch_array($resultJobDetail)
+                          ?>
+
+                          <td><?php echo $rowJobDetail['JOB_TITLE']; ?></td>
+                          <?php 
+                          $sqlRoundDetail = "SELECT * FROM INTERVIEW_ROUND WHERE ROUND_ID='".$data['ROUND_ID']."'";
+
+                          $resultRoundDetail = mysqli_query($conn, $sqlRoundDetail);
+                          $rowRoundDetail = mysqli_fetch_array($resultRoundDetail)
+                          ?>
+                          <td><?php echo $rowRoundDetail['ROUND_NAME']; ?></td>
+                          <?php 
+                          $sqlStatusDetail = "SELECT * FROM INTERVIEW_STATUS WHERE STATUS_ID='".$data['STATUS_ID']."'";
+
+                          $resultStatusDetail = mysqli_query($conn, $sqlStatusDetail);
+                          $rowStatusDetail = mysqli_fetch_array($resultStatusDetail)
+                          ?>
+                          <td><?php echo $rowStatusDetail['STATUS']; ?></td>
+                          <td><?php echo $data['STATUS_DATE']; ?></td>
                           <td>
                                     <a href="#" class="view-tag" ><img src="images/ico_view.png" alt="User Avatar"></a>
                           
@@ -61,26 +98,9 @@ include("header.php");
                                     <a href="#"><img src="images/ico_delete.png" alt="User Avatar"></a>
                           </td>
                       </tr>
-                      <tr>
-                          <td>1</td>
-                          <td>1</td>
-                          <td>Amit Rana</td>
-                          <td class="email-link">amit@gmail.com</td>
-                          <td>2 yrs</td>
-                          <td>898978979</td>
-                          <td>
-                                    <a href="#" class="view-tag" ><img src="images/ico_view.png" alt="User Avatar"></a>
-                          
-                          
-                                    <a href="#" class="edit-tag" ><img src="images/ico_edit.png" alt="User Avatar"></a>
-                          
-                        
-                                    <a href="#"><img src="images/ico_delete.png" alt="User Avatar"></a>
-                          </td>
-                      </tr>
-                    
-                   
                       
+                   
+                      <?php $i++; }  ?>
                     </tbody>
                     
         
