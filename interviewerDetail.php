1d2f52aeb0126b480f494417f2b3f32aa857ea54
gitpriyachaudhary <priya.chaudhary@aimbeyond.com>
2018-05-21 15:18:51 +0530

    chnges

--- interviewerDetail.php
+++ interviewerDetail.php
@@ -89,7 +89,7 @@ include("header.php");
                                 </div>
                                 
                                 <div class="col-lg-6 col-md-6 col-sm-6">   
-                                    <button type="submit" class="submitmaster">SUBMIT</button>
+                                    <button type="submit" class="submitmaster" name= "submit">SUBMIT</button>
                                 </div>
                             </div>
                         </form>
