b1fd43deefac7ee70b3aa4167979ddb727907da8
gitgauravtiwari <ggaurav.ttiwari@gmail.com>
2018-05-23 17:15:05 +0530

    conn

--- connection.php
+++ connection.php
@@ -1,11 +1,7 @@
  <?php
 $servername = "localhost";
 $username = "root";
-<<<<<<< HEAD
 $password = "root";
-=======
-$password = "dv@123";
->>>>>>> 5806e76c5b0e6d9061059bd89b9d5576b6b49ce2
 $dbname="rp_project";
 // Create connection
  $conn=@mysqli_connect($servername, $username, $password,$dbname);
