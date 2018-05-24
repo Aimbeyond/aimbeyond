a92632ee4e607e2be73d8405e71c981a7235b8c4
gitdivyanshu <divyanshubhardwaj4gmail.com>
2018-05-24 16:55:03 +0530

    new change

--- getApplicantName.php
+++ getApplicantName.php
@@ -4,7 +4,7 @@ if($_POST['id'])
 {
 	$id=$_POST['id'];
     
-    $jobsqsl="select a.*,b.*,c.* from APPLICANT_DETAIL as a join JOB_APPLY as b  on a.REG_ID=b.REG_ID join APPLICANT_STATUS as c on a.REG_ID=c.REG_ID  where b.JOB_ID=$id  &&c.STATUS_ID !=2";
+    $jobsqsl="select a.*,b.* from APPLICANT_DETAIL as a join JOB_APPLY as b  on a.REG_ID=b.REG_ID  where b.JOB_ID=$id ";
     //echo $jobsqsl; die();
     $result=mysqli_query($conn,$jobsqsl);
       
